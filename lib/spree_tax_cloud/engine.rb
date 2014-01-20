e917581f3af160de1feb2103de3953373b17dff2
brianthecoder <brian@20jeans.com>
2014-01-20 11:03:53 -0800

    config setup tweak

--- lib/spree_tax_cloud/engine.rb
+++ lib/spree_tax_cloud/engine.rb
@@ -11,6 +11,17 @@ module SpreeTaxCloud
       g.test_framework :rspec
     end
 
+    initializer "spree.shipstation.preferences", before: :load_config_initializers do |app|
+      Spree::AppConfiguration.class_eval do
+        preference :taxcloud_api_login_id, :string
+        preference :taxcloud_api_key, :string
+        preference :taxcloud_product_tic, :string, default: '00000'
+        preference :taxcloud_shipping_tic, :string, default: '11010'
+        preference :taxcloud_usps_user_id, :string
+        preference :taxcloud_origin, :string, default: {}.to_json
+      end
+    end
+
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
