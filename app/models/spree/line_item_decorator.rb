dfbbc6e9055a740e18890f1575040723953916a3
brianthecoder <brian@20jeans.com>
2014-03-01 08:12:27 -0800

    1.9 hash syntax

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,3 +1,3 @@
 Spree::LineItem.class_eval do
-  has_one :tax_cloud_cart_item, :class_name => 'Spree::TaxCloudCartItem', :dependent => :destroy
+  has_one :tax_cloud_cart_item, class_name: 'Spree::TaxCloudCartItem', dependent: :destroy
 end
